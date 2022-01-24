bd221923525f653af5514301b0932a4bb92e1671
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-24 17:16:54 -0500

    added persistence controller

--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import CoreData
+
+struct PersistenceController{
+    static let shared = PersistenceController()
+    
+    let container:NSPersistentContainer
+    init(inMemory: Bool = false){
+        container = NSPersistentContainer(name: "CDCRUDMVVM")
+        if inMemory{
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.loadPersistentStores(completionHandler: {(storeDescription, error) in
+                                       if let error = error as NSError? {
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+                                       }
+        )
+    }
+}
