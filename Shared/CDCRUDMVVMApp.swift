e51efb71758f9184f1bbdb0b721b28021cc7fdba
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-24 17:20:08 -0500

    added view model and connected it to whole app

--- Shared/CDCRUDMVVMApp.swift
+++ Shared/CDCRUDMVVMApp.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 @main
-struct CDCRUDMVVMApp: App {
+struct CD_CRUD_MVVMApp: App {
+    let persistenceController = PersistenceController.shared
+    @StateObject var taskListViewModel = TaskListViewModel()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(taskListViewModel)
         }
     }
 }
---
+++ Shared/TaskListViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  TaskListViewModel.swift
+//  CDCRUDMVVM
+//
+//  Created by Vincent Cloutier on 2022-01-24.
+//
+
+
+import Foundation
+import Combine
+import CoreData
+
+class TaskListViewModel: ObservableObject {
+    @Published var taskListTitle = ""
+    @Published var taskListItem:TaskList!
+    
+    func createTask(context:NSManagedObjectContext) {
+        if taskListItem == nil {
+            let task = TaskList(context: context)
+            task.id = UUID()
+            task.title = taskListTitle
+            task.isDone = false
+            task.isFavorite = false
+            task.date = Date()
+        }
+        else {
+            taskListItem.title = taskListTitle
+        }
+        
+        save(context: context)
+        taskListTitle = ""
+    }
+    
+    func editList(task:TaskList) {
+        taskListItem = task
+    }
+    
+    func delete(task:TaskList, context:NSManagedObjectContext) {
+        context.delete(task)
+        save(context: context)
+    }
+    
+    func isFavorite(task:TaskList, context:NSManagedObjectContext) {
+        task.isFavorite.toggle()
+        save(context: context)
+    }
+    
+    func isDone(task:TaskList, context:NSManagedObjectContext) {
+        task.isDone.toggle()
+        save(context: context)
+    }
+    
+    
+    func save(context:NSManagedObjectContext) {
+        do {
+            try context.save()
+        }
+        catch {
+            print(error)
+        }
+    }
+}
