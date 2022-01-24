4738397e50d228e758c353793e3be294758ab30e
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-24 17:13:29 -0500

    Initial Commit

---
+++ Shared/CDCRUDMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDCRUDMVVMApp.swift
+//  Shared
+//
+//  Created by Vincent Cloutier on 2022-01-24.
+//
+
+import SwiftUI
+
+@main
+struct CDCRUDMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
